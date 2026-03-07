5141af3e830f3d60a3c9ea26c7ad60ea8cd29721
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-07 14:19:16 +0530

    Added port

--- index.js
+++ index.js
@@ -77,4 +77,10 @@ app.post('/edit', function(req,res){
     });
 
 });
-app.listen(3000);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
